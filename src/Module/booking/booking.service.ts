5416197db969c43280178ce7a2b06a0537b21e7b
CHAN Suvannet <chansuvannet999@gmail.com>
2025-06-12 08:19:12 +0700

    update check id hotel is exit

--- src/Module/booking/booking.service.ts
+++ src/Module/booking/booking.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { Booking } from 'src/models/booking.model';
+import { Hotel } from 'src/models/hotel.model';
 import { CreateBookingInput } from './booking.dto';
 
 @Injectable()
 export class BookingService {
   private bookings: Booking[] = [];
+  private hotels: Hotel[] = [];
   private id = 1;
 
   create(input: CreateBookingInput): Booking {
-    const booking: Booking = {
-      id: this.id++,
-      ...input,
-      is_checked_in: false,
-    };
-    this.bookings.push(booking);
-    return booking;
+  const hotel = this.hotels.find(h => h.id === input.hotel_id);
+
+  if (!hotel) {
+    throw new Error(`Hotel with id ${input.hotel_id} does not exist.`);
   }
+
+  const booking: Booking = {
+    id: this.id++,
+    ...input,
+    is_checked_in: false,
+  };
+
+  this.bookings.push(booking);
+  return booking;
+}
+
 
   cancel(id: number): boolean {
     const idx = this.bookings.findIndex(b => b.id === id);
